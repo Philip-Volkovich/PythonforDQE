701ee4246f31a27d60f90edd6348fa702df515d9
Philip Volkovich <philip_volkovich@epam.com>
2023-10-12 16:43:40 +0400

    Added HW8 (utilits/json_parser)

---
+++ HW8/runner.py
@@ -0,0 +1,111 @@
+import sys
+import os
+from feed_class.news_feed_parent import NewsFeed
+from feed_class.news import News
+from feed_class.currency_conv import CurrencyConversion
+from feed_class.private_ad import PrivateAd
+from utilits.source_parser import TextParser
+from utilits.csv_creator import CsvCreator
+from utilits.json_parser import JsonParser
+
+
+class AppRunner:
+
+    def __init__(self):
+        self.source_files = 'source_files'
+
+    def console_runner(self):
+        """Run the console-based news feed application."""
+
+        while True:
+            print("\nSelect the way you want add data:")
+            print("1. Console mode")
+            print("2. Load from txt")
+            print("3. Load from json")
+            print("4. Exit")
+            main_choice = input("Enter your choice: ")
+
+            if main_choice == '1':
+                while True:
+                    print("\nSelect type of record to add:")
+                    print("1. News")
+                    print("2. Private Advertisement")
+                    print("3. Currency Rate")
+                    print("4. Exit")
+                    choice = input("Enter your choice: ")
+
+                    if choice == '1':
+                        # news_feed = NewsFeed('news')
+                        # news_feed.add_record()
+                        # news_feed.add_publication()
+                        input_text = input('Enter the news text: ')
+                        city = input('Enter the news city: ')
+                        news_feed = NewsFeed('news')
+                        news_feed.record = News(input_text, city)
+                        news_feed.add_publication()
+                    elif choice == '2':
+                        input_text = input('Enter the private advertisement text: ')
+                        expir_date = input('Enter expiration date (YYYY-MM-DD): ')
+                        news_feed = NewsFeed('private advertisement')
+                        news_feed.record = PrivateAd(input_text, expir_date)
+                        news_feed.add_publication()
+                    elif choice == '3':
+                        currency_from = input('Enter FROM which currency you want to convert (3 capital letters, e.g., USD): ')
+                        currency_to = input('Enter TO which currency you want to convert (3 capital letters, e.g., USD): ')
+                        rate = float(input('Enter currency exchange rate (integer or decimal): '))
+                        city = input('Enter city for currency exchange rate: ')
+                        news_feed = NewsFeed('currency conversion rate')
+                        news_feed.record = CurrencyConversion(currency_from, currency_to, rate, city)
+                        news_feed.add_publication()
+                    elif choice == '4':
+                        print("Exiting the console mode.")
+                        break
+                    else:
+                        print('Invalid choice. Please select a valid option.')
+            elif main_choice == '2':
+                file_name = input("Enter the name of the text file: ")
+                file_path = input('''Enter the path of the text file ends with file folder name
+                                  (press Enter for default path):''').strip()
+
+                if not file_path:
+                    file_path = os.path.join(sys.path[0], self.source_files)
+
+                full_file_path = os.path.join(file_path, file_name)
+
+                if os.path.exists(full_file_path):
+                    text_parser = TextParser(file_name)
+                    text_parser.parse_from_text(file_path)
+                    text_parser.line_parser()
+                else:
+                    print(
+                        f"The specified file '{full_file_path}' does not exist. Please provide a valid file path.")
+
+            elif main_choice == '3':
+                file_name = input("Enter the name of the JSON file: ")
+                file_path = input('''Enter the path of the JSON file ends with file folder name
+                                  (press Enter for default path):''').strip()
+
+                if not file_path:
+                    file_path = os.path.join(sys.path[0], self.source_files)
+
+                full_file_path = os.path.join(file_path, file_name)
+
+                if os.path.exists(full_file_path):
+                    json_parser = JsonParser(file_name)
+                    json_parser.load_from_json(file_path)
+                    json_parser.parse_from_json()
+                else:
+                    print(
+                        f"The specified file '{full_file_path}' does not exist. Please provide a valid file path.")
+            elif main_choice == '4':
+                print('Exiting the program')
+                break
+
+        csv_creator = CsvCreator(sys.path[0])
+        csv_creator.word_counter()
+        csv_creator.letter_count()
+
+
+if __name__ == "__main__":
+    run_app = AppRunner()
+    run_app.console_runner()
---
+++ HW8/utilits/source_parser.py
@@ -0,0 +1,70 @@
+import os
+from feed_class.news_feed_parent import NewsFeed
+from feed_class.news import News
+from feed_class.private_ad import PrivateAd
+from feed_class.currency_conv import CurrencyConversion
+from utilits.text_functions import convert_to_normalized_case
+
+
+class TextParser:
+    def __init__(self, file_name):
+        """Initialize a TextParser instance.
+
+        Args:
+        file_name (str): The name of the text file to parse."""
+        self.file_name = file_name
+        self.path = None
+        self.full_path = None
+        self.parsed_content = None
+
+    def parse_from_text(self, path=None):
+        """Parse content from a text file.
+
+        Args:
+            path (str, optional): The path to the text file. Defaults to None.
+        """
+        self.path = path
+        self.full_path = os.path.join(self.path, self.file_name)
+
+        with open(f'{self.full_path}', 'r') as file:
+            parsed_content = file.read().strip()
+            self.parsed_content = parsed_content.split('/')
+
+    def line_parser(self):
+        """Parse and process each line of content from the file."""
+
+        for record_str in self.parsed_content:
+            record_dict = {}
+            pairs_list = record_str.strip().split(';')
+            for pair in pairs_list:
+                pair = pair.strip().split(':')
+                if len(pair) == 2:
+                    key = pair[0].strip()
+                    value = pair[1].strip()
+                    record_dict[key] = value
+                    operation_type = record_dict.get('operation_type')
+
+            if operation_type == 'news':
+                news_feed = NewsFeed('news')
+                news_feed.record = News(convert_to_normalized_case(record_dict.get('input_text')),
+                                        convert_to_normalized_case(record_dict.get('city')))
+                news_feed.add_publication()
+            elif operation_type == 'private advertisement':
+                news_feed = NewsFeed('private advertisement')
+                news_feed.record = PrivateAd(convert_to_normalized_case(record_dict.get('input_text')),
+                                             record_dict.get('expir_date'))
+                news_feed.add_publication()
+            elif operation_type == 'currency_conversion':
+                news_feed = NewsFeed('currency_conversion')
+                news_feed.record = CurrencyConversion(convert_to_normalized_case(record_dict.get('from_currency')),
+                                                      convert_to_normalized_case(record_dict.get('to_currency')),
+                                                      record_dict.get('exchange_rate'),
+                                                      convert_to_normalized_case(record_dict.get('city')))
+                news_feed.add_publication()
+            else:
+                print(f'Invalid record: {record_str}. Please check the file.')
+
+            try:
+                os.remove(self.full_path)
+            except FileNotFoundError:
+                pass
